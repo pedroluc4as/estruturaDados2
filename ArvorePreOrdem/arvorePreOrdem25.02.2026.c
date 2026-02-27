3e3a58ab47f708b5771a64f278006387f0f13b24
pedroluc4as <ordepsperandio@hotmail.com>
2026-02-27 15:48:55 -0300

    first commit

---
+++ ArvorePreOrdem/arvorePreOrdem25.02.2026.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No
+{
+    int valor;
+    struct No *esquerda;
+    struct No *direita;
+} No;
+
+No *criarNo(int valor)
+{
+    No *novoNo = (No *)malloc(sizeof(No));
+    if (novoNo == NULL)
+    {
+        printf("Erro de alocacao de memoria!\n");
+        printf("Fechando o programa...\n");
+        return 0;
+    }
+    novoNo->valor = valor; //novoNo recebe o valor passado como parametro
+    novoNo->esquerda = NULL;
+    novoNo->direita = NULL;
+    //define a esq e direita da arvore como NULL,
+    //pois o No ainda nao possui filhos pois acabou de ser criado!
+    return novoNo; //retorna o novo no Vazio, 
+    //porem apenas com o valor do No passado por parametro
+}
+
+void preOrdem(No *raiz)
+{
+    if (raiz != NULL)
+    {
+        printf("%d ", raiz->valor);
+        preOrdem(raiz->esquerda);
+        preOrdem(raiz->direita);
+        free(raiz);
+    }
+}
+
+int main()
+{
+    No *raiz = criarNo(50);
+    raiz->esquerda = criarNo(28);
+    raiz->direita = criarNo(80);
+    raiz->esquerda->esquerda = criarNo(12);
+    raiz->direita->direita = criarNo(96);
+    raiz->esquerda->direita = criarNo(39);
+    raiz->direita->direita->direita = criarNo(100);
+
+
+    printf("Valores em pre ordem: ");
+    preOrdem(raiz);
+    printf("\n");
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Estrutura de Dados 2
+
+Arquivos e projetos desenvolvidos durante o semestre sobre ED2.
+
+## 🛠 Tecnologias Usadas
+- C
+
+<p align="center">
+  <img src="./images/header.png" alt="Banner Estrutura de Dados 2" width="100%">
+</p>
